7a1d71f16d4f904341712045dd02cc5fd641ec5a
Sergio <thesergioliveira@gmail.com>
2021-05-26 23:24:05 +0200

    updating the layout

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,5 @@
-// Any code you will write for your website (in the future!) should go here
+// DOM Selectors;
+
+const track = document.querySelector(".section-gallery-wrapper");
+const buttonRings = documetn.querySelector(".button-rings-carossel")
+const buttonBracelet = documetn.querySelector(".button-bracelet-carossel") 
